9c85595b2cbc4d9b0a9929f48bd448cdfbfb67e0
aabhapingle123 <61047894+aabhapingle@users.noreply.github.com>
2025-05-01 17:33:10 -0700

    final compilation new branch created

--- main.py
+++ main.py
@@ -147,13 +147,23 @@ print_metrics(rr_result, "Round Robin")
 # --- PPO (Single-Agent) ---
 ppo_result = train_and_run_ppo(copy.deepcopy(procs), retrain=False)
 print_metrics(ppo_result, "PPO")
-# plot_gantt_chart(ppo_result, title="PPO Scheduling Gantt Chart")
+plot_gantt_chart(ppo_result, title="PPO Scheduling Gantt Chart")
 
 
 # --- MAPPO (Multi-Agent) ---
 mappo_result = train_and_run_mappo(copy.deepcopy(procs), retrain=False, n_cores=4)
 print_metrics(mappo_result, "MAPPO")
 plot_gantt_chart(mappo_result, title="MAPPO Scheduling Gantt Chart")
+
+# dqn and qmix
+dqn_result = train_and_run_dqn(copy.deepcopy(procs), retrain=False)
+print_metrics(dqn_result, "DQN")
+#plot_gantt_chart(ppo_result, title="DQN Scheduling Gantt Chart")
+
+qmix_result = train_and_run_qmix(copy.deepcopy(procs), retrain=False)
+print_metrics(qmix_result, "QMIX")
+plot_gantt_chart(qmix_result, title="QMIX Scheduling Gantt Chart")
+
 
 
 # --- Plot all results ---
